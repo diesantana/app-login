26e500fba121f6ee774fd99c503bb1d6d86f1f42
Diego Santana <3c.santana@gmail.com>
2024-06-30 10:22:47 -0300

    feat: Implements Login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+}
