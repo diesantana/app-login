adae4b3080f8aae6a815b3404454a0504cf1bb9f
Diego Santana <3c.santana@gmail.com>
2024-07-08 08:47:59 -0300

    feat: Implements TokenService

---
+++ backend/src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -0,0 +1,62 @@
+package com.example.login_auth_api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.login_auth_api.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+// Service responsável por gerar e validar o token
+@Service
+public class TokenService {
+    
+    @Value("${api.security.token.secret}") // pega p valor secret do application.properties
+    private String secret;
+    
+    // Gera o token
+    public String generateToken(User user) {
+        try {
+            // Algortimo de geração do token
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            // Gerando o token
+            String token = JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException e) {
+            // O recomendado é lançar um exceção personalizada
+            throw new RuntimeException("Error while authentication");
+        }
+    }
+
+    // Valida o token
+    public String validateToken(String token) {
+        try {
+            // Algortimo de geração do token
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            
+            // Valida o token
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject(); // retorna o Subject (Email do user)
+        } catch (JWTVerificationException e) {
+            // Retorna nulo se o token for inválido
+            return null;
+        }
+    }
+    
+    // Define a duração do token
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
