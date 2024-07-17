30bf95398e518de156b52e538b0adeda6e9ec829
Diego Santana <3c.santana@gmail.com>
2024-07-17 11:54:55 -0300

    refactor: Refactor the package exceptions

--- backend/src/main/java/com/example/login_auth_api/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/example/login_auth_api/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.example.login_auth_api.services.exceptions;
+package com.example.login_auth_api.exceptions;
 // Classe de exceção personalizada para recursos não encontrados
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
