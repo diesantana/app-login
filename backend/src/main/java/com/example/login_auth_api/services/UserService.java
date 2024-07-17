dc209c0629b30d6178cb554240f16fd026013454
Diego Santana <3c.santana@gmail.com>
2024-07-17 10:54:18 -0300

    refactor: Refactor UserService

--- backend/src/main/java/com/example/login_auth_api/services/UserService.java
+++ backend/src/main/java/com/example/login_auth_api/services/UserService.java
@@ -6,12 +6,14 @@ import com.example.login_auth_api.repositories.UserRepository;
 import com.example.login_auth_api.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
     
+    @Transactional(readOnly = true)
     public UserDTO findByEmail(String email) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
